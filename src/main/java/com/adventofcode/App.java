cced3edc244cc2b867e4ccfbb3b4f2fc52ae20be
David McBride <dnmcbride@live.co.uk>
2023-12-10 23:42:25 +0000

    Change object name

--- src/main/java/com/adventofcode/App.java
+++ src/main/java/com/adventofcode/App.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 public class App {
     public static void main( String[] args ) throws IOException {
         System.out.println("Hello");
-        Day1 solution = new Day1(INPUT_SET.A);
-        String result = solution.solve();
+        Day1 day1= new Day1(INPUT_SET.A);
+        String result = day1.solve();
         System.out.println(result);
 
     }
