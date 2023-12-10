3c8a0e6393b91c5e03a31caae32fde82a8626266
David McBride <dnmcbride@live.co.uk>
2023-12-10 23:43:10 +0000

    Remove sout statement

--- src/main/java/com/adventofcode/App.java
+++ src/main/java/com/adventofcode/App.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 
 public class App {
     public static void main( String[] args ) throws IOException {
-        System.out.println("Hello");
         Day1 day1 = new Day1(INPUT_SET.A);
         String result = day1.solve();
         System.out.println(result);
